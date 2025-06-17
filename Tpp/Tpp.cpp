9ad5ec9049d1ab5cb404cfe2aed3139b17cc9d6a
Ofek Lavi <flikhamud123@gmail.com>
2025-06-17 14:28:01 +0300

    Revert "fix"
    This reverts commit cac65cc93a2fb71d5ec07dc835651502c3113ee6.

--- Tpp/Tpp.cpp
+++ Tpp/Tpp.cpp
@@ -3,21 +3,18 @@
 
 #include <iostream>
 
-const int MIN_MUL = 1;
-const int MAX_MUL = 11;
-const char* SEP = "\t";
+#define MIN_MUL 1
+#define MAX_MUL 11
+#define SEP "\t"
 
-void print_headline()
+int main()
 {
     for (int i = MIN_MUL; i <= MAX_MUL; i++)
     {
         std::cout << SEP << i;
     }
     std::cout << "\n";
-}
 
-void print_mul_board()
-{
     for (int i = MIN_MUL; i <= MAX_MUL; i++)
     {
         std::cout << i << SEP;
@@ -29,9 +26,14 @@ void print_mul_board()
     }
 }
 
-int main()
-{
-    print_headline();
-    print_mul_board();
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 
-}
+// Tips for Getting Started:
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files
+//   to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
