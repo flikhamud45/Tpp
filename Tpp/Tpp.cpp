b2b998e50d7a7280378a3a7be79be06df34b1720
Ofek Lavi <flikhamud123@gmail.com>
2025-06-18 10:09:56 +0300

    Revert "solve"
    This reverts commit d67ddb831a773a1db87017575ef98d8cfb2823ec.

--- Tpp/Tpp.cpp
+++ Tpp/Tpp.cpp
@@ -1,12 +1,7 @@
-#pragma once
 #include <iostream>
-#include "string.h"
 
 int main()
 {
-    String s1 = "hello";
-    std::cout << s1 << " world!\n";
-    std::cout << s1 + " world!\n";
 
     return 0;
 }
