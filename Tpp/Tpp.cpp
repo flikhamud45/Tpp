f957570b87619ff57c03a5b7b9a4977b3bd1d380
Ofek Lavi <flikhamud123@gmail.com>
2025-06-18 10:14:39 +0300

    solve
    This reverts commit b2b998e50d7a7280378a3a7be79be06df34b1720.

--- Tpp/Tpp.cpp
+++ Tpp/Tpp.cpp
@@ -1,7 +1,12 @@
+#pragma once
 #include <iostream>
+#include "string.h"
 
 int main()
 {
+    String s1 = "hello";
+    std::cout << s1 << " world!\n";
+    std::cout << s1 + " world!\n";
 
     return 0;
 }
---
+++ Tpp/string.cpp
@@ -0,0 +1,50 @@
+#include <cstring>
+#include "string.h"
+
+
+
+String::String() : String("")
+{
+	// left empty intentialy
+}
+
+String::String(const char* s)
+{
+    size_t len = strlen(s);
+    arr = new char[len+1];
+    strcpy_s(arr, len + 1, s);
+}
+
+String::String(const String& s)
+{
+    size_t len = strlen(s.arr);
+    arr = new char[len + 1];
+    strcpy_s(arr, len+1, s.arr);
+}
+
+String::~String()
+{
+    delete[] arr;
+}
+
+String String::operator+(const char* s)
+{
+    return *this + String(s);
+}
+
+String String::operator+(String s)
+{
+    char* new_arr = new char[strlen(arr) + strlen(s.arr) + 1];
+    strcpy_s(new_arr, strlen(arr)+1, arr);
+    strcat_s(new_arr, strlen(arr) + strlen(s.arr) + 1, s.arr);
+    String ans = String(new_arr);
+    delete[] new_arr;
+    return ans;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& str)
+{
+    os << str.arr;
+    return os;
+}
+
