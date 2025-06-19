b43f417a11ef3c86d878df202867e30c1dd0d49f
Ofek Lavi <flikhamud123@gmail.com>
2025-06-19 13:01:45 +0300

    Revert "temp"
    This reverts commit 815a99a93bed3c374f49ee7185e564022498a3ec.

--- Tpp/Tpp.cpp
+++ Tpp/Tpp.cpp
@@ -1,26 +1,7 @@
-#include <algorithm>
 #include <iostream>
-
-using namespace std;
-
-string xor_string(string s, int x)
-{
-    return [s, x]() { string s2; transform(s.begin(), s.end(), back_inserter(s2), [x](char c) { return static_cast<char>(c ^ x); });return s2;}();
-}
-
-string reverse_string(string s)
-{
-    return [s]() {
-        string s2;
-        transform(s.rbegin(), s.rend(), back_inserter(s2), [](char c) { return c; });
-        return s2;
-    }();
-}
 
 int main()
 {
-    cout << xor_string("hi", 0) << endl;
-    cout << reverse_string("hello") << endl;
 
     return 0;
 }
